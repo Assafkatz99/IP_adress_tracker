95442b51b93bb8f3ec334aed119efd81f14dd7a4
assafkatz99 <assafkatz99@gmail.com>
2023-01-24 17:07:00 +0200

    2nd commit

--- main.js
+++ main.js
@@ -1,69 +1,31 @@
+let map;
 
-const output_ex = {
-    "ip": "8.8.8.8",
-    "location": {
-        "country": "US",
-        "region": "California",
-        "city": "Mountain View",
-        "lat": 37.40599,
-        "lng": -122.078514,
-        "postalCode": "94043",
-        "timezone": "-07:00",
-        "geonameId": 5375481
-    },
-    "domains": [
-        "0d2.net",
-        "003725.com",
-        "0f6.b0094c.cn",
-        "007515.com",
-        "0guhi.jocose.cn"
-    ],
-    "as": {
-        "asn": 15169,
-        "name": "Google LLC",
-        "route": "8.8.8.0/24",
-        "domain": "https://about.google/intl/en/",
-        "type": "Content"
-    },
-    "isp": "Google LLC"
+function createMap(lang, lng) {
+  if (map) {
+    map.remove();
+  }
+  
+  const mapDiv = document.getElementById("map");
+  map = L.map(mapDiv).setView([lang+0.002, lng], 15);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
+  }).addTo(map);
+    
+  L.marker([lang, lng]).addTo(map);
+
 }
-
-const buildMap = (lat,lng) => {
-    
-
-
-    // let map = L.map('map').setView([lat+0.003, lng], 15);
-    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     maxZoom: 19,
-    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    // }).addTo(map);
-
-    // let marker = L.marker([lat, lng]).addTo(map);
-    
-    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    osmAttribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
-    ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    osmLayer = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
-    var map = new L.Map('map');
-    map.setView(new L.LatLng(lat,lng), 15 );
-    map.addLayer(osmLayer);
-    let marker = L.marker([lat, lng]).addTo(map);
-    
-    
-}
-
 
 const getIpDataAndPutInDisplay = async (ip_input) => {
     
 // ######################### Pulling IP information #########################
 
     let ip_data;
-    // await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_AHrDRbJAZkNbupmUCVakYg50sQqIn&ipAddress=${ip_input}`)
-    // .then(response => response.json())
-    // .then(data_api => ip_data = data_api)
-    // .catch(error => console.error(error));
-
-    ip_data = output_ex;
+    await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_AHrDRbJAZkNbupmUCVakYg50sQqIn&ipAddress=${ip_input}`)
+    .then(response => response.json())
+    .then(data_api => ip_data = data_api)
+    .catch(error => console.error(error));
 
 
 // ######################### Put information inside modal #########################
@@ -74,25 +36,26 @@ const getIpDataAndPutInDisplay = async (ip_input) => {
 
 // ######################### Put information inside map #########################
 
-buildMap(ip_data["location"]["lat"], ip_data["location"]["lng"])
+createMap(ip_data["location"]["lat"], ip_data["location"]["lng"])
 
 
 }
 
 
-buildMap(32.524529, 34.941971) // Binyamina's location
+createMap(32.524529, 34.941971) // Binyamina's location
 
 document.getElementById("search_button_id").addEventListener("click", () => {
 
     let input_bar = document.getElementById("search_input_id")
 
     var ipFormat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    // if (ipFormat.test(input_bar.value)){
+    
+    if (ipFormat.test(input_bar.value)){
     getIpDataAndPutInDisplay(input_bar.value)
         input_bar.value = ""
-    // }else{
-    //     alert("please type again proper IP address")
-    // }
+    }else{
+        alert("please type again proper IP address")
+    }
     
 })
 
